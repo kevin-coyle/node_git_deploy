e5a6bcd48b6878e4d36f6038a365698f000f2221
Kevin Coyle <kevin.coyle@drutek.co.uk>
2015-02-17 00:11:51 +0000

    Added first test of route

--- test/test.js
+++ test/test.js
@@ -1,2 +1,23 @@
-var assert = require("assert");
+var sinon = require('sinon');
+var chai = require('chai');
+var expect = chai.expect;
+var config = require('../config.json');
 
+var deploymentAction = require('../lib/deployment.js').do;
+
+describe("Routes", function() {
+    describe("POST Deployment", function() {
+        it("should respond when the code is correct", function(){
+            var req,res,spy;
+
+            req = res = {};
+            //set a blank object for the params too;
+            req.params = {};
+            req.params.code = config.secure_code;
+            spy = res.send = sinon.spy();
+
+            deploymentAction(req, res);
+            expect(spy.calledOnce).to.equal(true);
+        })
+    });
+});
